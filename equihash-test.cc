52b29f79bfbf64b37324d455647ff9ff19e4e069
stf <7o5rfu92t@ctrlc.hu>
2021-04-15 19:50:50 +0000

    [mod] make benchmark compile on arm

--- equihash-test.cc
+++ equihash-test.cc
@@ -27,6 +27,22 @@ static uint64_t rdtsc(void) {
     uint64_t rax;
     __asm__ __volatile__("rdtsc" : "=A"(rax) : : );
     return rax;
+#elif defined(__ARM_ARCH) && (__ARM_ARCH >= 6)  // V6 is the earliest arch that has a standard cyclecount
+  return 0;
+  uint32_t pmccntr;
+  uint32_t pmuseren;
+  uint32_t pmcntenset;
+  // Read the user mode perf monitor counter access permissions.
+  asm volatile("mrc p15, 0, %0, c9, c14, 0" : "=r"(pmuseren));
+  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+    asm volatile("mrc p15, 0, %0, c9, c12, 1" : "=r"(pmcntenset));
+    if (pmcntenset & 0x80000000ul) {  // Is it counting?
+      asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(pmccntr));
+      // The counter is set up to count every 64th cycle
+      return static_cast<int64_t>(pmccntr) * 64;  // Should optimize to << 6
+    }
+  }
+  return 0;
 #else
 #error "Not implemented!"
 #endif
